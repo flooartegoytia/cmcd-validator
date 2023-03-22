b03d6d108a763b2727987972c5b80dbe8c73e208
flooartegoytia <florenciaa@qualabs.com>
2023-03-21 21:03:00 -0300

    base

--- packages/cmcd-validator-demo-app/src/components/ShakaExample.jsx
+++ packages/cmcd-validator-demo-app/src/components/ShakaExample.jsx
@@ -6,6 +6,7 @@ import "./ShakaExample.css";
 import { DataWindow } from "./DataWindow";
 import { ValidatorView } from "./ValidatorView";
 import { CMCDQueryValidator } from "@montevideo-tech/cmcd-validator";
+import VideoJS from "./VideojsExample";
 
 export function ShakaExample() {
   window.muxjs = muxjs;
@@ -20,45 +21,78 @@ export function ShakaExample() {
   };
 
   const onButtonClick = () => {
-    const {
-      /** @type {shaka.Player} */ player,
-      /** @type {shaka.ui.Overlay} */ ui,
-      /** @type {HTMLVideoElement} */ videoElement,
-    } = controllerRef.current;
+    // const {
+    //   /** @type {shaka.Player} */ player,
+    //   /** @type {shaka.ui.Overlay} */ ui,
+    //   /** @type {HTMLVideoElement} */ videoElement,
+    // } = controllerRef.current;
 
-    player.configure({
-      cmcd: {
-        contentId: "testContentId",
-        enabled: true,
-        sessionId: "testSessionId",
-        useHeaders: false,
-      },
-    });
-    const networkEngine = player.getNetworkingEngine();
-    networkEngine.registerRequestFilter((type, request) => {
-      const newUris = [{}];
-      const urisToAdd = [...new Set(request.uris)];
-      for (const uri of urisToAdd) {
-        newUris.push({ url: uri, result: CMCDQueryValidator(uri) });
-      }
-      setNewData(newUris);
-    });
-    async function loadAsset() {
-      // Load an asset.
-      try {
-        await player.load(url);
-      } catch (error) {
-        console.log(error);
-      }
-      // Trigger play.
-      videoElement.play();
-    }
-    loadAsset();
+    // player.configure({
+    //   cmcd: {
+    //     contentId: "testContentId",
+    //     enabled: true,
+    //     sessionId: "testSessionId",
+    //     useHeaders: false,
+    //   },
+    // });
+    // const networkEngine = player.getNetworkingEngine();
+    // networkEngine.registerRequestFilter((type, request) => {
+    //   const newUris = [{}];
+    //   const urisToAdd = [...new Set(request.uris)];
+    //   for (const uri of urisToAdd) {
+    //     newUris.push({ url: uri, result: CMCDQueryValidator(uri) });
+    //   }
+    //   setNewData(newUris);
+    // });
+    // async function loadAsset() {
+    //   // Load an asset.
+    //   try {
+    //     await player.load(url);
+    //   } catch (error) {
+    //     console.log(error);
+    //   }
+    //   // Trigger play.
+    //   videoElement.play();
+    // }
+    // loadAsset();
   };
+
+  const videojsOptions = {
+    autoplay: true,
+    controls: true,
+    responsive: true,
+    fluid: true,
+    sources: [{
+      src: 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8',
+      type: 'application/x-mpegURL'
+    }]
+  }
+
+  const playerRef = React.useRef(null);
+
+  const handlePlayerReady = (player) => {
+    playerRef.current = player;
+
+    const vhs = player.tech(true).vhs;
+    console.log(vhs)
+
+    vhs.xhr.beforeRequest = function(options) {
+      console.log('entr1')
+      console.log(options)
+
+      return options;
+    }   
+
+    player.on("waiting", () => {
+      console.log("player is waiting");
+    });
+  };
+
+  
 
   return (
     <>
-      <div className="mb-4 contet input-group d-flex justify-content-center">
+      {/* <div className="mb-4 contet input-group d-flex justify-content-center">
         <input
           className="text ms-2"
           placeholder="Manifest URL"
@@ -67,11 +101,12 @@ export function ShakaExample() {
         <button className="btn btn-primary" onClick={onButtonClick}>
           Start Shaka
         </button>
-      </div>
+      </div> */}
       <div className="row">
         <div className="col-6">
           <div className="col-12 mb-4">
-            <ShakaPlayer ref={controllerRef} />
+            {/* <ShakaPlayer ref={controllerRef} /> */}
+            <VideoJS options={videojsOptions} onReady={handlePlayerReady}/>
           </div>
           <ValidatorView output={validatorOutput} />
         </div>
